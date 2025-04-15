feb80696b608a5f9aa96e738c2fc710e757a1334
Anglebase <1628818324@qq.com>
2025-04-15 18:52:49 +0800

    添加测试示例

--- src/generated.rs
+++ src/generated.rs
@@ -1,3 +1,16 @@
 use crate::types::*;
 
 include!(concat!(env!("OUT_DIR"), "/generated.rs"));
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_mat() {
+        let v = Vec4::from([[1, 2, 3, 4]]);
+        assert_eq!(v.xy(), Vec2::from([1, 2]));
+        assert_eq!(v.xyz(), Vec3::from([1, 2, 3]));
+        assert_eq!(v.xwzz(), Vec4::from([1, 4, 3, 3]));
+    }
+}
